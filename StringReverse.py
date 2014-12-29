913706956c02b9cfd12311798744a15e054758fb
Nickil <nickilsomanna@gmail.com>
2014-12-28 19:53:56 -0700

    Reverse a string
    Reversing a string in python

--- StringReverse.py
+++ StringReverse.py
@@ -1,1 +1,9 @@
-print "hello World"
+# Input variable to enter the string
+inp=raw_input("Enter the string \n")
+length=len(inp)
+print length
+revinp=''
+for i in range(length-1,-1,-1):
+ revinp=revinp+inp[i]
+print revinp
+ 
